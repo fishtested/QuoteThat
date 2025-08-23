e9e0b26f5f28b19ac0ca3eca441d2c66656a060c
fish <123904088+fishtested@users.noreply.github.com>
2025-08-22 21:17:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,44 @@ client.on('messageReactionAdd', async (reaction, user) => {
         img.fillRect(0, 0, canvas.width, canvas.height); // size
         img.fillStyle = '#000000'; // text colour
         img.font = '25px sans-serif'; // text font
-        img.fillText(quote.content, 20, 100); // text location
+        img.textAlign = 'center';
+        img.fillText(quote.content, canvas.width / 2, 40); // text location
+        img.textAlign = 'left';
+
+        let date = quote.createdAt;
+        let monthnum = date.getMonth();
+        let year = date.getFullYear();
+        let day = date.getDate();
+        let month
+
+        if (monthnum === 0) {
+            month = 'January';
+        } else if (monthnum === 1) {
+            month = 'February';
+        } else if (monthnum === 2) {
+            month = 'March';
+        } else if (monthnum === 3) {
+            month = 'April';
+        } else if (monthnum === 4) {
+            month = 'May';
+        } else if (monthnum === 5) {
+            month = 'June';
+        } else if (monthnum === 6) {
+            month = 'July';
+        } else if (monthnum === 7) {
+            month = 'August';
+        } else if (monthnum === 8) {
+            month = 'September';
+        } else if (monthnum === 9) {
+            month = 'October';
+        } else if (monthnum === 10) {
+            month = 'November';
+        } else if (monthnum === 11) {
+            month = 'December';
+        }
+
+
+        img.fillText(`${quote.member.displayName} - ${month} ${day}, ${year}`, 20, 380);
         const buffer = canvas.toBuffer('image/png');
         const attachment = new AttachmentBuilder(buffer, { name: `quote-${Date.now()}-${quote.guildId}.png` });
         await quote.channel.send({ files: [attachment] });
